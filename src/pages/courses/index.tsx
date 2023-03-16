7ccc4e6d135bd502f92941312da984a3404f7512
antonpalermo <ANPBusinessmail@gmail.com>
2023-03-16 19:23:25 +0800

    feat: create course

--- src/pages/courses/index.tsx
+++ src/pages/courses/index.tsx
@@ -6,28 +6,38 @@ import fetcher from "@libs/fetcher"
 
 export default function Courses() {
   const router = useRouter()
-  const { data, isLoading } = useSWR<Course[]>("/api/courses", fetcher)
+  const { data, isLoading, mutate } = useSWR<Course[]>("/api/courses", fetcher)
 
   if (isLoading) {
     return <h1>loading...</h1>
   }
 
+  function handleEditCourse(cid: string) {
+    router.push({
+      pathname: "/drafts/[cid]",
+      query: { cid }
+    })
+  }
+
+  async function handleCreateCourse() {
+    const course = await fetcher("/api/courses", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name: "Blank Course" })
+    })
+
+    handleEditCourse(course.id)
+    mutate([...data, course])
+  }
+
   return (
     <div>
       <h1>Courses</h1>
+      <button onClick={handleCreateCourse}>create</button>
       {data.map(course => (
         <div key={course.id}>
           {JSON.stringify(course)}
-          <button
-            onClick={() =>
-              router.push({
-                pathname: "/drafts/[cid]",
-                query: { cid: course.id }
-              })
-            }
-          >
-            edit
-          </button>
+          <button onClick={() => handleEditCourse(course.id)}>edit</button>
         </div>
       ))}
     </div>
