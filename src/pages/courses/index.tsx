85d2f0b1d0cf67351b4225504e68c96eb9379ac5
antonpalermo <ANPBusinessmail@gmail.com>
2023-04-01 22:08:47 +0800

    create courses

--- src/pages/courses/index.tsx
+++ src/pages/courses/index.tsx
@@ -1,4 +1,18 @@
+import fetcher from "@utils/fetcher"
+
 export default function Courses() {
+  async function handleCreate() {
+    const { data, error } = await fetcher("/api/courses", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({})
+    })
+
+
+    
+
+  }
+
   return (
     <div>
       <button>create</button>
