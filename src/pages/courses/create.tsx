a488283fa87e04be7f66254be5e8adb7fef31a26
antonpalermo <ANPBusinessmail@gmail.com>
2023-04-05 21:57:09 +0800

    refac: update create form

--- src/pages/courses/create.tsx
+++ src/pages/courses/create.tsx
@@ -1,41 +1,70 @@
 import fetcher from "@utils/fetcher"
+import { Form, Formik, FormikHelpers } from "formik"
 import { FormEvent } from "react"
 
+interface CourseDetails {
+  name: string
+  description: string
+}
+
 export default function CourseCreate() {
-  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault()
+  const initialData: CourseDetails = {
+    name: "",
+    description: ""
+  }
+
+  async function onSubmit(
+    values: CourseDetails,
+    helpers: FormikHelpers<CourseDetails>
+  ) {
     const { data, error } = await fetcher("/api/courses", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        name: "Introduction to APIs",
-        description: "A complete introduction to computer programming!"
+        name: values.name,
+        description: values.description
       })
     })
 
     if (error && error.code === 405) {
       console.log(data)
     }
-
-    console.log(data)
   }
 
   return (
     <div>
       <h1>Create new</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" />
-        </div>
-        <div>
-          <label htmlFor="name">Description</label>
-          <input type="text" name="description" id="description" />
-        </div>
-        <button type="submit">Create</button>
-      </form>
+      <Formik initialValues={initialData} onSubmit={onSubmit}>
+        {({ values, isSubmitting, handleChange }) => (
+          <Form>
+            <div>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                value={values.name}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="name">Description</label>
+              <input
+                type="text"
+                name="description"
+                id="description"
+                value={values.description}
+                onChange={handleChange}
+              />
+            </div>
+            <button type="submit" disabled={isSubmitting}>
+              Create
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   )
 }
--- src/pages/courses/index.tsx
+++ src/pages/courses/index.tsx
@@ -1,21 +1,22 @@
 import fetcher from "@utils/fetcher"
+import { useRouter } from "next/router"
 
 export default function Courses() {
+  const router = useRouter()
+
   async function handleCreate() {
     const { data, error } = await fetcher("/api/courses", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({})
     })
-
-
-    
-
   }
 
   return (
     <div>
-      <button>create</button>
+      <button onClick={() => router.push({ pathname: "/courses/create" })}>
+        create
+      </button>
       <h1>Courses</h1>
     </div>
   )
