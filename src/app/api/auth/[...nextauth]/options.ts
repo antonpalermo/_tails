522b2ceb771bab33dc49e1482cd55b3c85e92e88
antonpalermo <ANPBusinessmail@gmail.com>
2024-01-25 23:54:13 +0800

    auth: added adapters

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,8 +1,11 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import { NextAuthOptions } from "next-auth"
 
+import prisma from "@/lib/prisma"
 import GoogleProvider from "next-auth/providers/google"
 
 const options: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
